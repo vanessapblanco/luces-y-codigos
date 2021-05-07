cef9a9c2fa9781b8aab0ee1b339942ee0ec9cc4b
vanessapblanco <68451502+vanessapblanco@users.noreply.github.com>
2021-05-07 08:07:44 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,160 @@
+/**
+ * Código MakeCode Ficha 1 
+ * 
+ * Actividad conectada Unidad 1
+ * 
+ * Vanessa Paola Blanco Villafañe
+ */
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+})
